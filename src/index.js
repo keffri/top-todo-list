fea194b86f4401d3367f15987e441dda8a0b73c2
keffri <keffri.w.neal@gmail.com>
2021-03-11 18:50:59 -0800

    webpack

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import projectDOM from "./modules/projectDOM";
+import projectLogic from "./modules/projectCreate";
+
+const todoListTitle = document.querySelector(".headerTitle");
+todoListTitle.addEventListener("click", () => {
+  console.log(projectLogic.projectList);
+});
 
 const newProjectButton = document.getElementById("newProjectButton");
-
 newProjectButton.addEventListener("click", projectDOM.projectBox);
